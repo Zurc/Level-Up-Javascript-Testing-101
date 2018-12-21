bab2e6e1d2d0197335597d6e3785b8d0005b91d6
Zurc <cruzjurado@gmail.com>
2018-12-21 12:07:05 +0000

    multiple test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { hello } from './App';
+import { hello, add } from './App';
 
 describe('hello', () => {
   // what IT should do
   it('should output hello', () => {
     expect(hello()).toBe('Hello');
-  })  
+  });
+});
+
+describe('add', () => {
+  // you can have different test cases, if one fails IT should fail
+  it('should add two numbers', () => {
+    expect(add(1,2)).toBe(3);
+    expect(add(2,2)).toBe(4);
+    expect(add(12,2)).toBe(14);
+    expect(add(-2,2)).toBe(0);
+    expect(add(-2,-2)).toBe(-4);
+  });
+  it('should not add strings', () => {
+    expect(add(2, "2")).toBe(null);
+  });
+  it('should not add objects', () => {
+    expect(add(2, {})).toBe(null);
+  });
+  it('should not add arrays', () => {
+    expect(add(2, [])).toBe(null);
+  });
 });
