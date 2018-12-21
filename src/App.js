8ebe5e26244b8d988dc6bc02506cad3145919f27
Zurc <cruzjurado@gmail.com>
2018-12-21 13:00:21 +0000

    TDD and refactoring

--- src/App.js
+++ src/App.js
@@ -6,3 +6,22 @@ export const add = (x,y) => {
   }
   return x + y;
 }
+
+export const removeSNames = names => {
+  // TDD approach, start with null, build the test and then write some code
+  // return null;
+
+  // initial idea, check that passes the tests
+  // let newNames = names;
+  // names.forEach(name => {
+  //   if(name.toLowerCase().charAt(0) === 's') {
+  //     // find the position
+  //     const index = newNames.indexOf(name);
+  //     newNames.splice(index, 1);
+  //   }
+  // })
+  // return newNames;
+
+  // new idea, refactoring Vue.config.keyCodes = {
+    return names.filter(name => name.toLowerCase().charAt(0) !== 's');
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { hello, add } from './App';
+import { hello, add, removeSNames } from './App';
 
 describe('hello', () => {
   // what IT should do
@@ -26,3 +26,22 @@ describe('add', () => {
     expect(add(2, [])).toBe(null);
   });
 });
+
+describe('removeSNames', () => {
+  it('should remove all s names', () => {
+    const names = ['Scott', 'Courtney', 'Steve']
+    expect(removeSNames(names)).not.toContain('Scott');
+    expect(removeSNames(names)).not.toContain('Steve');
+  });
+  it('should not remove other names', () => {
+    const names = ['Scott', 'Courtney', 'Wes']
+    expect(removeSNames(names)).toContain('Courtney');
+    expect(removeSNames(names)).toContain('Wes');
+  });
+  it('should account for upper and lower cases', () => {
+    const names = ['Scott', 'Courtney', 'Wes', 'scott']
+    expect(removeSNames(names)).not.toContain('Scott');
+    expect(removeSNames(names)).not.toContain('scott');
+  });
+})
+
